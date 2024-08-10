14ffa48a87d147f860dfdc56647649b9fed67706
DhinakaranDev <166213395+DhinakaranDev@users.noreply.github.com>
2024-08-11 00:38:17 +0530

    feat : base setup created
    1.Created folder structure(src,components,utils,screens)
    2.Created basic components
    3.Created files for colors,strings to manage globally

---
+++ src/components/CustomTextInput.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const CustomTextInput = (props: Props) => {
+  return (
+    <View>
+      <Text>CustomTextInput</Text>
+    </View>
+  );
+};
+
+export default CustomTextInput;
+
+const styles = StyleSheet.create({});
---
+++ src/components/Loder.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const Loder = (props: Props) => {
+  return (
+    <View>
+      <Text>Loder</Text>
+    </View>
+  );
+};
+
+export default Loder;
+
+const styles = StyleSheet.create({});
