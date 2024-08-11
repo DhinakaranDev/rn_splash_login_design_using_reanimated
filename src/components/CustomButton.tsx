779ffb9db8834c201ef67679cdda8d90c2350f86
DhinakaranDev <166213395+DhinakaranDev@users.noreply.github.com>
2024-08-11 14:18:38 +0530

    feat : created necessary components
    1. updated CustomBotton.tsx
    2.updated CustomText.tsx
    3.updated CustomTextInput.tsx
    4.updated Loader.tsx

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,16 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Text, TouchableHighlight, StyleSheet } from "react-native";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import colors from "../utils/colors";
+interface CustomButtonProps {
+  title: string;
+  onPress: () => void;
+}
 
-type Props = {};
-
-const CustomButton = (props: Props) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ title, onPress }) => {
   return (
-    <View>
-      <Text>CustomButton</Text>
-    </View>
+    <TouchableHighlight
+      style={styles.loginButton}
+      onPress={onPress}
+      underlayColor="#3ea963"
+    >
+      <Text style={styles.loginButtonText}>{title}</Text>
+    </TouchableHighlight>
   );
 };
 
+const styles = StyleSheet.create({
+  loginButton: {
+    width: "100%",
+    backgroundColor: colors.primary,
+    borderRadius: 11,
+    marginVertical: hp("2%"),
+  },
+  loginButtonText: {
+    color: colors.buttonText,
+    textAlign: "center",
+    paddingVertical: hp("1.3%"),
+    fontWeight: "500",
+  },
+});
 export default CustomButton;
-
-const styles = StyleSheet.create({});
--- src/components/CustomTextInput.tsx
+++ src/components/CustomTextInput.tsx
@@ -1,16 +1,54 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { HelperText, TextInput } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
+import colors from "../utils/colors";
 
-type Props = {};
+interface CustomTextInputProps {
+  label: string;
+  value: string;
+  hasErrors: boolean;
+  errorText: string;
+  onChangeText: (text: string) => void;
+  secureTextEntry?: boolean;
+}
 
-const CustomTextInput = (props: Props) => {
+const CustomTextInput: React.FC<CustomTextInputProps> = ({
+  label,
+  value,
+  onChangeText,
+  errorText,
+  secureTextEntry = false,
+  hasErrors,
+}) => {
   return (
     <View>
-      <Text>CustomTextInput</Text>
+      <TextInput
+        label={label}
+        style={styles.input}
+        textColor={colors.secondary}
+        placeholderTextColor={colors.secondary}
+        value={value}
+        secureTextEntry={secureTextEntry}
+        underlineColor={colors.secondary}
+        activeUnderlineColor={colors.secondary}
+        onChangeText={onChangeText}
+      />
+      {hasErrors && <HelperText type="error">{errorText}</HelperText>}
     </View>
   );
 };
 
 export default CustomTextInput;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    width: wp("80%"),
+    paddingHorizontal: wp("3%"),
+    backgroundColor: "transparent",
+    marginTop: hp("2.5%"),
+  },
+});
