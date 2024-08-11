9ebea7945b1a155d08f08d456d0f7aec5b9919a5
DhinakaranDev <166213395+DhinakaranDev@users.noreply.github.com>
2024-08-11 15:33:47 +0530

    feat : splas screen added
    1.installed react native npm packages
    2.added necessary plugin in the babel.config.js file

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,16 +1,64 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { NavigationProp, useFocusEffect } from "@react-navigation/native";
+import Animated, {
+  BounceInDown,
+  BounceInUp,
+  PinwheelIn,
+} from "react-native-reanimated";
+import colors from "../utils/colors";
 
-type Props = {};
+type Props = {
+  navigation: NavigationProp<any, any>;
+};
 
 const Splash = (props: Props) => {
-  return (
-    <View>
-      <Text>Splash</Text>
+  const [initalAnim, setInitialAnim] = useState(true);
+  useFocusEffect(() => {
+    setTimeout(() => setInitialAnim(false), 1000);
+    setTimeout(() => props.navigation.navigate("Login"), 3000);
+  });
+
+  return initalAnim ? (
+    <>
+      <Animated.View
+        entering={BounceInDown}
+        exiting={BounceInUp}
+        style={styles.topContainer}
+      ></Animated.View>
+      <Animated.View
+        entering={BounceInUp}
+        exiting={BounceInDown}
+        style={styles.bottomContainer}
+      />
+    </>
+  ) : (
+    <View style={styles.mainContainer}>
+      <Animated.Image
+        entering={PinwheelIn.duration(500).delay(500)}
+        source={require("../assets/splash_logo.png")}
+        style={{ width: 150, height: 150 }}
+        sharedTransitionTag="tag"
+      />
     </View>
   );
 };
 
 export default Splash;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.primary,
+  },
+  topContainer: {
+    flex: 0.5,
+    backgroundColor: colors.primary,
+  },
+  bottomContainer: {
+    flex: 0.5,
+    backgroundColor: colors.primary,
+  },
+});
